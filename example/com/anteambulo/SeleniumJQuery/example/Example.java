913f87d4bc6d5118244b69112e3ddcb144c0a54c
Carl Taylor <phocis@gmail.com>
2012-01-24 22:40:20 -0800

    Google's pages do not work with HtmlUnitDriver anymore?

--- example/com/anteambulo/SeleniumJQuery/example/Example.java
+++ example/com/anteambulo/SeleniumJQuery/example/Example.java
@@ -2,18 +2,13 @@ package com.anteambulo.SeleniumJQuery.example;
 
 import java.util.concurrent.TimeoutException;
 
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import com.anteambulo.SeleniumJQuery.jQueryFactory;
-import com.gargoylesoftware.htmlunit.BrowserVersion;
 
 public class Example extends jQueryFactory {
   public static void main(String[] args) throws TimeoutException {
-    // HtmlUnitDriver is loud...
-    System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.SimpleLog");
-    System.setProperty("org.apache.commons.logging.simplelog.log.com.gargoylesoftware.htmlunit", "fatal");
-    HtmlUnitDriver drv = new HtmlUnitDriver(BrowserVersion.FIREFOX_3_6);
-    drv.setJavascriptEnabled(true);
+    FirefoxDriver drv = new FirefoxDriver();
     try {
       Example jq = new Example();
       jq.setJs(drv);
