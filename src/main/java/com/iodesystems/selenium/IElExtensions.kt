d67c0cd108fde992018d0aa8fb1115aa3517fb0d
Carl Taylor <carl@etaylor.me>
2025-03-18 12:43:15 -0700

    feat: enhance IElExtensions with icontains method and add switchToTab functionality

--- src/main/java/com/iodesystems/selenium/IElExtensions.kt
+++ src/main/java/com/iodesystems/selenium/IElExtensions.kt
@@ -1,14 +1,15 @@
 package com.iodesystems.selenium
 
 import com.iodesystems.selenium.jQuery.IEl
+import org.openqa.selenium.remote.RemoteWebDriver
 
 object IElExtensions {
   fun IEl.button(text: String): IEl {
-    return find("button").contains(text)
+    return find("button").icontains(text)
   }
 
   fun IEl.link(text: String): IEl {
-    return find("a").contains(text)
+    return find("a").icontains(text)
   }
 
   fun IEl.linkTo(href: String): IEl {
@@ -18,6 +19,31 @@ object IElExtensions {
   fun IEl.waitForPageLoaded() {
     waitFor("document.readyState === 'complete'") {
       if (js("return document.readyState") == "complete") true else null
+    }
+  }
+
+  fun IEl.not(selector: String): IEl {
+    return refine(":not($selector)")
+  }
+
+  fun IEl.switchToTab(label: String, cb: IEl.(IEl) -> Unit) {
+    val jq = jq()
+    val driver = waitFor {
+      jq.driver.windowHandles.stream().map { handle ->
+        val newDriver = jq.driver.switchTo().window(handle)
+        val title = newDriver.title ?: ""
+        if (title.contains(label, ignoreCase = true)) {
+          newDriver
+        } else {
+          null
+        }
+      }.filter { it != null }.findFirst().orElse(null)
+    }
+    jq.copy(
+      driver = driver as RemoteWebDriver,
+    ).find("html").apply {
+      cb(this)
+      driver.close()
     }
   }
 }
