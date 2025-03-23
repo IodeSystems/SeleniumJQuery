d222d146f078f1c4068966d4c70a048cc2010333
Carl Taylor <carl@etaylor.me>
2025-03-22 19:21:02 -0700

    Release 3.2.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.time.Duration
 
 group = "com.iodesystems.selenium-jquery"
-version = "3.2.2-SNAPSHOT"
+version = "3.2.2"
 description =
   "SeleniumJQuery is a tool for writing more effective Selenium tests with the power of jQuery selectors and Kotlin's expressiveness"
 
