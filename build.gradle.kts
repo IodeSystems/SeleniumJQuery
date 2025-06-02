2ddc2aee794ae3738ee568f32f647b8f49e3a3d1
Carl Taylor <carl@etaylor.me>
2025-06-02 11:25:30 -0700

    Prepare next development iteration: 4.1.3-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.time.Duration
 
 group = "com.iodesystems.selenium-jquery"
-version = "4.1.2"
+version = "4.1.3-SNAPSHOT"
 description =
   "SeleniumJQuery is a tool for writing more effective Selenium tests with the power of jQuery selectors and Kotlin's expressiveness"
 
